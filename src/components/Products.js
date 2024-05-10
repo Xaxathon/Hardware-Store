f58c3a0b7428cd555035002d40406197eaaa89c4
Askh <susuev.95a@mail.ru>
2024-05-10 23:11:45 +0300

    update list

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,52 +2,102 @@ import styled from "styled-components";
 import ProductItem from "./ProductItem";
 import { useContext } from "react";
 import CartContext from "../store/cart-context";
+import { useState } from "react";
 
 const TitleDiv = styled.div`
-  margin-top: 8rem;
-  & h3 {
-    margin: 0.5rem auto;
-  }
-  & p {
-    margin: 0.9em auto;
-    color: var(--grey);
-  }
+	margin-top: 8rem;
+	& h3 {
+		margin: 0.5rem auto;
+	}
+	& p {
+		margin: 0.9em auto;
+		color: var(--grey);
+	}
 `;
 
-const ProductItemDiv = styled.div` 
-    display: flex;
-    flex-direction: column;
-    margin: 3.7rem auto;
-    gap: 2.5rem;
-    
-    @media (min-width: 580px) {
-        grid-template-columns: repeat(2, 1fr) ;
-        display: grid;
-    }
-    
-    @media (min-width: 768px) {
-        grid-template-columns: repeat(3, 1fr) ;
-    }
-`
+const ProductItemDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 3.7rem auto;
+	gap: 2.5rem;
 
-const Products = () => {
-  const {cartState} = useContext(CartContext);
+	@media (min-width: 580px) {
+		grid-template-columns: repeat(2, 1fr);
+		display: grid;
+	}
 
-  return (
-    <section>
-      <TitleDiv>
-        <h3>Все товары</h3>
-        <p>Лучшие товары по лучшим ценам, если они вам нужны, они у нас есть.
-</p>
-      </TitleDiv>
-      <ProductItemDiv>
-        {cartState.items.map((e) => (
-          <ProductItem key={e.id} item={e} />
-        ))}
-      </ProductItemDiv>
-    </section>
-  );
+	@media (min-width: 768px) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+`;
+
+const Button = styled.button`
+	padding: 10px 20px;
+	margin: 0 5px;
+	background-color: ${({ active }) => (active ? "darkorange" : "white")};
+	color: ${({ active }) => (active ? "white" : "black")};
+	border: 1px solid gray;
+	cursor: pointer;
+
+	&:hover {
+		background-color: ${({ active }) => (active ? "darkorange" : "darkorange")};
+		color: white;
+	}
+`;
+
+const Products = ({ DUMMY_LIST }) => {
+	const { cartState, cartDispatch } = useContext(CartContext);
+	const [activeCategory, setActiveCategory] = useState("ALL");
+
+	const handleCategoryChange = (category) => {
+		setActiveCategory(category);
+		cartDispatch({ type: "FILTER_ITEMS", payload: category });
+	};
+	return (
+		<section>
+			<TitleDiv>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+					}}
+				>
+					<div>
+						<h3>Все товары</h3>
+						<p>
+							Лучшие товары по лучшим ценам, если они вам нужны, они у нас есть.
+						</p>
+					</div>
+					<div>
+						<Button
+							active={activeCategory === "ALL"}
+							onClick={() => handleCategoryChange("ALL")}
+						>
+							Все
+						</Button>
+						<Button
+							active={activeCategory === "GARDEN"}
+							onClick={() => handleCategoryChange("GARDEN")}
+						>
+							Садовая
+						</Button>
+						<Button
+							active={activeCategory === "HOUSEHOLD"}
+							onClick={() => handleCategoryChange("HOUSEHOLD")}
+						>
+							Бытовая
+						</Button>
+					</div>
+				</div>
+			</TitleDiv>
+			<ProductItemDiv>
+				{cartState.items.map((e) => (
+					<ProductItem key={e.id} item={e} />
+				))}
+			</ProductItemDiv>
+		</section>
+	);
 };
 
 export default Products;
-
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,76 +1,120 @@
-import React, { useReducer } from 'react';
-import DUMMY_LIST from '../DUMMY_LIST';
+import React, { useReducer } from "react";
+import DUMMY_LIST from "../DUMMY_LIST";
 
 const CartContext = React.createContext({
-  cartState: {
-    items: [],
-    cart: [],
-    showModal: false,
-  },
-  cartDispatch: undefined,
+	cartState: {
+		items: [],
+		cart: [],
+		showModal: false,
+	},
+	cartDispatch: undefined,
 });
 
 const cartInitialState = {
-  items: [...DUMMY_LIST],
-  cart: [],
-  showModal: false,
+	items: [...DUMMY_LIST],
+	cart: [],
+	showModal: false,
+};
+
+const categoryFilters = {
+	ALL: () => true, // Показать все товары
+	GARDEN: (item) =>
+		[
+			"Тачка",
+			"Лопатка",
+			"Электрическая дрель",
+			"Беспроводной молоток",
+			"Угловая шлифмашина",
+			"Лобзик",
+			"Стационарная пила",
+			"Шлифовальная машина",
+		].includes(item.title), // Садовые товары
+	HOUSEHOLD: (item) =>
+		[
+			"Дрель",
+			"Угловая шлифовальная машина",
+			"Электрическая дрель",
+			"Шлифовальная машина",
+			"Набор гаечных ключей",
+			"Покрасочный аппарат",
+			"Сварочный аппарат",
+		].includes(item.title), // Бытовые товары
+	TOOLS: (item) =>
+		[
+			"Пилка",
+			"Отвертки",
+			"Электрическая дрель",
+			"Беспроводной молоток",
+			"Циркулярная пила",
+			"Угловая шлифмашина",
+			"Лобзик",
+			"Стационарная пила",
+		].includes(item.title), // Инструменты
 };
 
 const cartReducer = (state, action) => {
-  switch (action.type) {
-    case 'TOGGLE_CART':
-      return { ...state, showModal: !state.showModal };
+	switch (action.type) {
+		case "TOGGLE_CART":
+			return { ...state, showModal: !state.showModal };
 
-    case 'UPDATE_ITEMS':
-      const updatedItem = state.items.map( e=> e.id === action.payload.id ? action.payload : e)
-      return { ...state, items: updatedItem };
+		case "UPDATE_ITEMS":
+			const updatedItem = state.items.map((e) =>
+				e.id === action.payload.id ? action.payload : e
+			);
+			return { ...state, items: updatedItem };
 
-    case 'UPDATE_CART':
-      return { ...state, cart: action.payload ? action.payload : [] };
+		case "UPDATE_CART":
+			return { ...state, cart: action.payload ? action.payload : [] };
 
-    case 'ADD_CART':
-      const existCart = state.cart.find((e) => e.id === action.payload.id);
-      let newCart = [];
-      if (existCart) {
-        newCart = state.cart.map((e) =>
-          e.id !== action.payload.id
-            ? e
-            : { ...e, quantity: e.quantity + action.payload.quantity }
-        );
-      } else {
-        newCart = [...state.cart, action.payload];
-      }
-      return { ...state, cart: newCart };
+		case "ADD_CART":
+			const existCart = state.cart.find((e) => e.id === action.payload.id);
+			let newCart = [];
+			if (existCart) {
+				newCart = state.cart.map((e) =>
+					e.id !== action.payload.id
+						? e
+						: { ...e, quantity: e.quantity + action.payload.quantity }
+				);
+			} else {
+				newCart = [...state.cart, action.payload];
+			}
+			return { ...state, cart: newCart };
 
-    case 'UPDATE_ITEM_CART':
-      const updatedItemCart = state.cart.map((e) =>
-          e.id !== action.payload.id
-            ? e
-            : { ...e, quantity: action.payload.quantity }
-        );
-      return { ...state, cart: updatedItemCart };
+		case "UPDATE_ITEM_CART":
+			const updatedItemCart = state.cart.map((e) =>
+				e.id !== action.payload.id
+					? e
+					: { ...e, quantity: action.payload.quantity }
+			);
+			return { ...state, cart: updatedItemCart };
 
-    case 'DELETE_ITEM_CART':
-      const deletedItemCart = state.cart.filter((e) =>
-          e.id !== action.payload);
-      return { ...state, cart: deletedItemCart ? deletedItemCart : [] };
-    
-      case 'CLEAR_CART':
-      return { ...state, cart: [] };
+		case "DELETE_ITEM_CART":
+			const deletedItemCart = state.cart.filter((e) => e.id !== action.payload);
+			return { ...state, cart: deletedItemCart ? deletedItemCart : [] };
 
-    default:
-      return state;
-  }
+		case "CLEAR_CART":
+			return { ...state, cart: [] };
+		case "FILTER_ITEMS":
+			const category = action.payload;
+			const filterFunction = categoryFilters[category] || categoryFilters.ALL;
+			return {
+				...state,
+				items: DUMMY_LIST.filter(filterFunction),
+			};
+
+		default:
+			return state;
+	}
 };
 
 export const CartContextProvider = ({ children }) => {
-  const [cartState, cartDispatch] = useReducer(cartReducer, cartInitialState);
+	const [cartState, cartDispatch] = useReducer(cartReducer, cartInitialState);
 
-  return (
-    <CartContext.Provider value={{ cartState, cartDispatch }}>
-      {children}
-    </CartContext.Provider>
-  );
+	return (
+		<CartContext.Provider value={{ cartState, cartDispatch }}>
+			{children}
+		</CartContext.Provider>
+	);
 };
 
 export default CartContext;
